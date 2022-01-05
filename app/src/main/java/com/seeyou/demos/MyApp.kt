7b95f79017cd4f8815d04d2fe8f41ef29c05fcac
taosiyuan <taosiyuan@xiaomi.com>
2022-01-05 21:14:50 +0800

    first commit

---
+++ app/src/main/java/com/seeyou/demos/MyApp.kt
@@ -0,0 +1,17 @@
+package com.seeyou.demos
+
+import android.app.Application
+
+/**
+ * <pre>
+ *     author : tao
+ *     time   : 2022/01/05
+ * </pre>
+ */
+class MyApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+}
