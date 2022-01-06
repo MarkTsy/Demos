ec7919bd8170f101ac6b2f2fa16f55f0822fd8bf
Tsy <54782203@qq.com>
2022-01-06 20:21:34 +0800

    volley demo

--- volley/src/main/java/com/seeyou/volley/MainActivity.kt
+++ volley/src/main/java/com/seeyou/volley/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.seeyou.volley
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 
 class MainActivity : AppCompatActivity() {
+
+    private val TAG : String = javaClass.name
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Thread {
+
+            val newRequestQueue = Volley.newRequestQueue(this@MainActivity)
+            val stringRequest = StringRequest("http://www.baidu.com",
+                {
+                    Log.e(TAG, it)
+                }, {
+                    it.printStackTrace()
+                })
+
+            newRequestQueue.add(stringRequest)
+
+        }.start()
+
     }
 }
